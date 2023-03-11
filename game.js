f8ad2f5aca1f7b08fb97fb763ca532b68af8ebe7
Andreas <osdonline@gmail.com>
2023-03-11 15:43:35 +0100

    Switching over to a function that controls game state

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ const menuMusic = new Audio("menu.mp3");
 const bombSound = new Audio("bomb.wav");
 const pointSound = new Audio("point.mp3");
 const lifeSound = new Audio("lifegain.mp3");
+let stateController = "";
 
 
 function generateSprite(spriteType) {
@@ -15,7 +16,7 @@ function generateSprite(spriteType) {
 
     let spriteShape;
 
-    switch (spriteType) {
+    switch(spriteType) {
         case "heart":
             spriteShape = "❤️";
             break;
@@ -40,45 +41,78 @@ function addElement(injectionType, content, cssClass = 0) {
     return newElement;
 }
 
+function removeElement(elementSelector) {
+    document.querySelector(elementSelector).remove();
+}
+
 function generateRandomSprite(inputArray) {
     const randomIndex = Math.floor(Math.random() * inputArray.length);
     return inputArray[randomIndex];
 }
 
 function generateGameState() {
-    let num = 0;
-    while(num < 10) {
-        num++;
-        fragment.appendChild(generateSprite(generateRandomSprite(spriteObjects)));
+    function startingScreen() {
+        addElement("h1", "Clicky boom game", "h1");
+        addElement("div", "Welcome challenger - Press to play", "welcome");
+        //Note to self: Arrow function its like writing a function but in line instead of defining it on its own.
+        //There is a difference with how the this JS function works in an arrow function VS normal function 
+        //I am not sure I understand that exactly yet.
+        document.querySelector(".welcome").addEventListener("click", e => {
+            menuMusic.play();
+            menuMusic.loop = true;
+            stateController = "menuScreen";
+            generateGameState();
+        });
     }
 
-    addElement("h1", "Boom boom game");
-    // addElement("div", "Start game", "menu");
-    // addElement("div", "Options", "menu");
-    // addElement("div", "Exit game", "menu");
-    addElement("div", "Welcome challenger - Press to play", "welcome");
+    function menuScreen() {
+        removeElement(".welcome");
+        addElement("div", "Start game", "menu");
+        addElement("div", "Options", "menu");
+        addElement("div", "Exit game", "menu");
+        document.querySelectorAll(".menu").forEach(element => {
+            element.addEventListener("click", e => {
+                console.log(e);
+            });
+        });
+    }
+
+    function gameScreen() {
+        let num = 0;
+        while(num < 10) {
+            num++;
+            fragment.appendChild(generateSprite(generateRandomSprite(spriteObjects)));
+        }
+        document.querySelectorAll(".sprite-universals").forEach(element => {
+            element.addEventListener("click", e => {
+                console.log(e);
+                pointSound.play();
+            });
+        });
+    }
+
+    function gameOver() {
+        addElement("h1", "GAME OVER!", "h1")
+    }
+
+    switch(stateController) {
+        case "menuScreen":
+            menuScreen();
+            break;
+
+        case "gameOver":
+            gameOver();
+            break;
+
+        case "gameScreen":
+            gameScreen();
+        default:
+            startingScreen();
+            break;
+    }
 }
 
 generateGameState();
 
 //Note to self: DocumentFragment is for adding in sprites without reloading the entire DOM.
 gameBoard.appendChild(fragment);
-
-//Note to self: Arrow function its like writing a function but in line instead of defining it on its own.
-document.querySelectorAll(".sprite-universals").forEach(element => {
-    element.addEventListener("click", e => {
-        console.log(e);
-        pointSound.play();
-    });
-});
-
-document.querySelectorAll(".menu").forEach(element => {
-    element.addEventListener("click", e => {
-        console.log(e);
-    });
-});
-
-document.querySelector(".welcome").addEventListener("click", e => {
-    menuMusic.play();
-    menuMusic.loop = true;
-});
